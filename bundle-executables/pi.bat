cea8964efe4e1ef2cb03c0a8c605331b405ba640
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-07 20:26:57 +0000

    Minor changes

--- bundle-executables/pi.bat
+++ bundle-executables/pi.bat
@@ -1,5 +1,13 @@
 @echo off
-setlocal
+setlocal enableextensions enabledelayedexpansion
 set "DIR=%~dp0"
+
+where node >nul 2>&1
+if errorlevel 1 (
+	echo Error: Node.js is required but not found in PATH.
+	echo Install Node.js with: scoop install node (or use the installer from https://nodejs.org/)
+	exit /b 2
+)
+
 node "%DIR%cli-with-packages.js" %*
 exit /b %ERRORLEVEL%
